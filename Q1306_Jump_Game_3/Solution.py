2d7c3cde1c8514031324a446e1a943cf6bb46452
mzhai-UofC <37128021+mzhai-UofC@users.noreply.github.com>
2022-12-10 13:10:36 -0700

    Add files via upload

---
+++ Q1306_Jump_Game_3/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if arr[start] == 0:
+            return True
+
+        n = len(arr)
+        used = {start}
+        q = collections.deque([start])
+
+        while len(q) > 0:
+            u = q.popleft()
+            for v in [u + arr[u], u - arr[u]]:
+                if 0 <= v < n and v not in used:
+                    if arr[v] == 0:
+                        return True
+                    q.append(v)
+                    used.add(v)
+        
+        return False
